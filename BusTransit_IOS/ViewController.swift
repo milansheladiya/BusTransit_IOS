6d45e23cf840ce6de8935cd49bd6e8aac1d81d9e
milansheladiya007 <milansheladiya007@gmail.com>
2022-07-08 13:14:42 -0400

    first commit

---
+++ BusTransit_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusTransit_IOS
+//
+//  Created by Milan Sheladiya on 2022-07-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
