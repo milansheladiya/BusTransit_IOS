9c3684e19a91f7519e45ccbad266cae2cda5e920
Nency <patelnency37@gmail.com>
2022-08-16 10:08:17 -0400

    Reading single document from firebase for parent profile

---
+++ BusTransit_IOS/Controller/Parent/ParentProfileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ParentProfileViewController.swift
+//  BusTransit_IOS
+//
+//  Created by Nency Patel on 2022-08-16.
+//
+
+import UIKit
+
+class ParentProfileViewController: UIViewController {
+
+    let fb = FirebaseUtil()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+            
+        }
+        
+    }
+    
+
+
+}
